4796017da062ed43aa88a3b0a05f211d48585893
Bastian Eicher <bastian@eicher.net>
2018-06-08 17:08:06 +0200

    Switched to new MSBuild format

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 NDesk.Options
 =============
 
-Command-line parsing library with some custom patches.
+Command-line parsing library. Some custom patches and recompiled for .NET Standard.
 
 Code forked from http://www.ndesk.org/Options (original Git repository seems to be down)
---
+++ build.ps1
@@ -0,0 +1,10 @@
+﻿Param ($Version = "1.0-dev")
+$ErrorActionPreference = "Stop"
+pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+$vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath -format value
+$msBuild = "$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"
+
+. $msBuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=$Version src
+
+popd
