991f1e45551b6fa4e28f713e67c6629217902eb5
Bastian Eicher <bastian@eicher.net>
2021-01-05 14:12:38 +0100

    Replaced AppVeyor with GitHub Actions

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 NDesk.Options
 =============
 
-[![NDesk.Options.Patched NuGet](https://img.shields.io/nuget/v/NDesk.Options.Patched.svg)](https://www.nuget.org/packages/NDesk.Options.Patched/) [![Build status](https://img.shields.io/appveyor/ci/nano-byte/ndesk-options.svg)](https://ci.appveyor.com/project/nano-byte/ndesk-options)
+[![NDesk.Options.Patched NuGet](https://img.shields.io/nuget/v/NDesk.Options.Patched.svg)](https://www.nuget.org/packages/NDesk.Options.Patched/)
+[![Build](https://github.com/nano-byte/NDesk.Options/workflows/Build/badge.svg?branch=master)](https://github.com/nano-byte/NDesk.Options/actions?query=workflow%3ABuild)
 
 Command-line parsing library. Some custom patches and recompiled for .NET Standard.
 
--- build.ps1
+++ build.ps1
@@ -2,9 +2,22 @@
 $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
-$vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath -format value
-$msBuild = "$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"
+function Find-MSBuild {
+    if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe") {
+        $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products * -property installationPath -format value
+        if ($vsDir) {
+            if (Test-Path "$vsDir\MSBuild\Current") { return "$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe" } else { return "$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe" }
+        }
+    }
+}
 
-. $msBuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=$Version src
+function Run-MSBuild {
+    $msbuild = Find-Msbuild
+    if (!$msbuild) { throw "You need Visual Studio 2019 v16.5+ to build this project" }
+    . $msbuild @args
+    if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+}
+
+Run-MSBuild /v:Quiet /t:Restore /t:Build /p:Configuration=Release /p:Version=$Version src
 
 popd
